ebe9f13de9c63f86b2f8b2bac5cb9d264f282e39
kaichiro <kcfg23@gmail.com>
2019-10-04 09:15:32 -0300

    Adicionando rota para pegar arquivos estáticos

--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose')
-
 const cors = require('cors')
+const path = require('path');
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 3333;
-
 const dbName = process.env.DB_NAME;
 const dbPassword = process.env.DB_PASSWORD;
 const dbUser = process.env.DB_USER;
 const dbCluster = process.env.DB_CLUSTER;
-
 const dbCntStr = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}-mkbtw.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 mongoose.connect(dbCntStr, {
@@ -22,8 +20,9 @@ mongoose.connect(dbCntStr, {
 
 app.use(cors());
 app.use(express.json());
+app.use(`/files`, express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(port, () => {
-    console.log(`Backend is running at http://localhost:${port}`)
+    console.log(`${new Date()} - Backend is running at http://localhost:${port}`)
 });
